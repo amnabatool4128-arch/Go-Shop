666eb31512fd62eae811b13ebbb06e2fd9ea11c9
Amna Batool <amnabatool4128@gmail.com>
2026-03-30 22:09:30 +0500

    Navbar Resposive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,14 +16,14 @@ const Navbar = ({
       className={`bg-white fixed top-0 left-0 right-0 transition-all duration-300 z-30 ${isScrolled ? "shadow-xl" : ""}`}
     >
       {/* Logo */}
-      <nav className="max-w-[1400px] md:px-12 px-6 md:py-10 py-5 mx-auto flex items-center justify-between h-[12vh]  md:h-[14vh] gap-3">
-        <div className="flex items-center gap-2">
+      <nav className="max-w-[1400px] md:px-12 px-2 md:py-10 py-2 mx-auto flex items-center sm:flex-row flex-col justify-between h-[12vh] md:h-[14vh] gap-2">
+        <div className="flex items-center  gap-1">
           <h1 className="lg:text-2xl sm:text-xl font-bold text-[14px] tracking-wide">
             G<span className="text-blue-800">O</span>-Shop
           </h1>
           <a
             href="https://www.google.com"
-            className="flex w-8 h-8 sm:w-10 sm:h-10 bg-zinc-100 rounded-full p-2 "
+            className="flex w-8 h-8 sm:w-12 sm:h-12 bg-zinc-100 rounded-full p-1 "
           >
             <img
               src={Logo}
@@ -33,20 +33,20 @@ const Navbar = ({
           </a>
         </div>
         {/* Nav Action */}
-        <div className="flex items-center gap-x-5 sm:gap-x-2 ">
+        <div className="flex items-center gap-x-5 sm:gap-x ">
           {/* Search Box */}
-          <div className="flex p-1 rounded-full border-2 border-blue-600">
+          <div className="flex p-1 rounded-full px-1 py-1 border-2 border-blue-600">
             <input
               type="text"
               name="search"
               id="search"
-              placeholder="Search..."
+              placeholder=" Search..."
               autoComplete="off"
-              className="lg:h-[5vh] lg:pl-4 flex-1 bg-transparent focus:outline-none"
+              className="lg:h-[5vh] lg:pl-4 flex-1 sm:h-[2.5vh] bg-transparent focus:outline-none"
               onFocus={handleScroll}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <button className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-700 text-white text-lg transition-all duration-300  hover:scale-110">
+            <button className="flex items-center justify-center md:w-10 md:h-10 w-6 h-6 rounded-full bg-blue-700 text-white text-lg sm:text-xs transition-all duration-300  hover:scale-110">
               <FaSearch />
             </button>
           </div>
